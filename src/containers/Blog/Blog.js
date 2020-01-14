29c35130f29c8d73cde1f46d5b368b72292e0265
Rob Terrell <Rterrell25@gmail.com>
2020-01-14 10:35:41 -0500

    added an async hoc that allows my NewPosts component to load lazily, meaning that it only loads when the pages route has been selected as oppossed to loading everything within the application at once

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import Posts from "./Posts/Posts";
-import NewPost from "./NewPost/NewPost";
-
+// import NewPost from "./NewPost/NewPost";
+import asyncComponent from "../../hoc/asyncComponent.js";
 import { Route, NavLink, Switch } from "react-router-dom";
 import "./Blog.css";
 
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
+});
 class Blog extends Component {
   state = {
-    auth: false
+    auth: true
   };
 
   render() {
@@ -32,10 +35,10 @@ class Blog extends Component {
 
         <Switch>
           {this.state.auth ? (
-            <Route path="/new-post" component={NewPost} />
+            <Route path="/new-post" component={AsyncNewPost} />
           ) : null}
           <Route path="/posts" component={Posts} />
-          <Route render={() => <h1>Please Log In</h1>} />
+          <Route render={() => <h1>Not Found</h1>} />
           {/* <Redirect from="/" to="/posts" /> */}
         </Switch>
       </div>
